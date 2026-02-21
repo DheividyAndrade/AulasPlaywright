bcb218ad1d85411b71631b099506cd76f630a31f
Dheividy Andrade <deividyandrade@hotmail.com>
2026-02-21 18:14:21 -0300

    up
    up

--- aula1.py
+++ aula1.py
@@ -14,11 +14,12 @@ with sync_playwright() as p:
     page.goto('https://playground-devaprender.netlify.app/',wait_until='domcontentloaded')
     input('Digite ENTER para encerrar a automação')
     browser.close()
+'''
 
-
-''' 
+# Abrir um navegador persistente MELHOR OPÇÃO!!
 from playwright.sync_api import sync_playwright
-    # Abrir um navegador persistente 
+
+
 with sync_playwright() as p:
     browser = p.chromium.launch_persistent_context(
         user_data_dir=r'./automacao_whatsapp', # pasta onde os dados do navegador serão armazenados
@@ -27,6 +28,10 @@ with sync_playwright() as p:
         accept_downloads=True, # permite downloads automaticos
         downloads_path=r'./downloads', # pasta onde os arquivos baixados serão armazenados
         locale="pt-BR", # define o idioma do navegador para português do Brasil
+        record_video_dir=r'./videos', # define a pasta onde os videos gravados serão armazenados
+        record_video_size={"width": 1360, "height": 768}, # define o tamanho do video gravado
+        color_scheme="dark", # define o tema do navegador para escuro/ se quiser claro use "light"
+
     ) # Cria um novo (perfil persistente) para paginas, com configurações independentes, cookies e permissões    
 
     page = browser.new_page() # abre uma nova pagina (aba) no navegador
